3f7d66b9f0657b2e0ee0f632bc1115a860059477
e.tairov <ev.tairov@yandex.ru>
2023-02-01 02:37:47 +0700

    Home5_02

--- Home5_Task_02/Program.cs
+++ Home5_Task_02/Program.cs
@@ -5,3 +5,38 @@
 //[-4, -6, 89, 6] -> 0
 //-------------------------
 
+int[] GetArray(int newstring)
+{
+    int[] arr = new int[newstring];
+    Random rnd = new Random();
+
+    for (int i = 0; i < newstring; i++)
+    {
+        arr[i] = rnd.Next(-10, 10);
+    }
+    return arr;
+}
+
+void EvenOddNumber(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 > 0)
+        {
+          sum += array[i]++;
+        }
+    }
+    Console.WriteLine($"Кол-во элементов на нечетных позициях: {sum}");
+}
+
+int[] array = GetArray(4);
+
+Console.Write("Массив чисел на входе: [");
+Console.Write(string.Join(", ", array));
+Console.Write("]");
+
+Console.WriteLine();
+Console.WriteLine($"-------------------------------------------------------------------------------");
+
+EvenOddNumber(array);
